d5f87c2220745f4f01b52eee7e3da96c2c1e3e62
Ko-GyeongTae <kokt0203@naver.com>
2021-05-20 12:49:55 +0900

    Edit: Screen

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState, useRef } from 'react';
+import { StyleSheet, View, Platform, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={Style.Header}>
+        <Text style={FontStyle.Title}>고래산마을</Text>
+      </View>
+      <View style={Style.Body}>
+
+        <View style={{ paddingTop: 50, width: 400, height: 290 }}>
+          <WebView
+            javaScriptEnabled={true}
+            useWebKit={true}
+            domStorageEnabled={true}
+            allowsInlineMediaPlayback={true}
+            source={{ uri: "https://www.youtube.com/embed/TcMBFSGVi1c?playsinline=1&fs=1" }}
+          />
+        </View>
+      </View>
+      <View style={Style.Footer}>
+
+      </View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const FontStyle = StyleSheet.create({
+  Title: {
+    fontSize: 40,
+    paddingTop: 20,
   },
 });
+
+const Style = StyleSheet.create({
+  Header: {
+    backgroundColor: 'red',
+    width: '100%',
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Body: {
+    backgroundColor: 'orange',
+    width: '100%',
+    height: 700,
+  },
+  Footer: {
+    backgroundColor: 'yellow',
+    width: '100%',
+    height: 100,
+  }
+})
